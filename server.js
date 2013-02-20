8f51b0abda567218e88e6c388b7b6cc00e3c276c
Frode Fikke <frodefi@gmail.com>
2013-02-20 20:16:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var server = restify.createServer({
   name: 'json-server'
 });
 
-server.get('/vocababe/get/:id/:rows', getTranslations);
+server.get('/vocababe/get/:id/:rows', getTranslations;
 server.post('/vocababe/new', createTranslation);
 server.put('/vocababe/edit', updateTranslation);
 server.del('/vocababe/delete', deleteTranslation);
@@ -16,14 +16,36 @@ server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
 
-function getTranslations(req, res) {
-  var result = getTranslations(req.params.id,req.params.rows);
-  res.send(result);
+function getTranslationsById(req, res) {
+  res.send(db.getTranslations({
+    Dictionary: req.params.dictionary,
+    UserId: req.params.userId, // When a user creates his own or modifies a default translation
+    OriginalTranslationId: req.params.originalTranslationId, // then these 2 columns are used
+  }));
+}
+
+function getTranslationsByWord(req, res) {
+  res.send(db.getTranslations({
+    Dictionary: req.params.dictionary,
+    UserId: req.params.userId, // When a user creates his own or modifies a default translation
+    OriginalTranslationId: req.params.originalTranslationId, // then these 2 columns are used
+    FromWord: req.params.fromWord,
+    FromDescription: req.params.FromDescription,
+    ToWord: req.params.toWord,
+    ToDescription: req.params.toDescription
+  }));
 }
 
 function createTranslation(req, res) {
-  var result = createTranslations(req.body);
-  res.send(result);
+  res.send(db.getTranslations({
+    Dictionary: req.params.dictionary,
+    UserId: req.params.userId, // When a user creates his own or modifies a default translation
+    OriginalTranslationId: req.params.originalTranslationId, // then these 2 columns are used
+    FromWord: req.params.fromWord,
+    FromDescription: req.params.FromDescription,
+    ToWord: req.params.toWord,
+    ToDescription: req.params.toDescription
+  }));
 }
 
 function updateTranslation(req, res, next) {
